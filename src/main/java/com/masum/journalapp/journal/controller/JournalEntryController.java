20c2893798ebce5d892616fcdf400040df31a67c
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-09-27 17:35:39 +0530

    POST request working

--- src/main/java/com/masum/journalapp/journal/controller/JournalEntryController.java
+++ src/main/java/com/masum/journalapp/journal/controller/JournalEntryController.java
@@ -1,9 +1,7 @@
 package com.masum.journalapp.journal.controller;
 
 import com.masum.journalapp.journal.entity.JournalEntry;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -17,8 +15,14 @@ public class JournalEntryController {
 
     private Map<Long, JournalEntry> journalEntries = new HashMap<>();
 
-    @GetMapping("/all")
+    @GetMapping
     public List<JournalEntry> getAllEntries() {
         return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean postEntry(@RequestBody JournalEntry journalEntry) {
+        journalEntries.put(journalEntry.getId(), journalEntry);
+        return true;
     }
 }
