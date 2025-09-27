dd30350b400bcc9ae904aa28084ff2e290de39a6
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-09-27 15:31:00 +0530

    Initials

---
+++ src/test/java/com/masum/journalapp/journal/JournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masum.journalapp.journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
