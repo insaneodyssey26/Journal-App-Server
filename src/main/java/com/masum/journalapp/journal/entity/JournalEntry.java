d9f47ca2f00f3692c3404fadd331d442068159ca
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-10-01 15:45:45 +0530

    Added Lombok dependency to auto-generate getters and setters

--- src/main/java/com/masum/journalapp/journal/entity/JournalEntry.java
+++ src/main/java/com/masum/journalapp/journal/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package com.masum.journalapp.journal.entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,43 +9,12 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document (collection = "journal_entries")
+@Data
 public class JournalEntry {
-
     @Id
     private ObjectId id;
     private String title;
     private String content;
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
