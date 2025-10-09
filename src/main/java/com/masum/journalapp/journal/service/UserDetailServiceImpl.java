c515fa621d9a418960312046c86857c66bfedc3d
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-10-09 21:55:14 +0530

    feat: user detail service implementation to fetch user details

---
+++ src/main/java/com/masum/journalapp/journal/service/UserDetailServiceImpl.java
@@ -0,0 +1,30 @@
+package com.masum.journalapp.journal.service;
+
+import com.masum.journalapp.journal.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.masum.journalapp.journal.entity.User user = userRepository.findByUsername(username);
+        if (user != null) {
+            UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
+                    .username(user.getUsername())
+                    .password(user.getPassword())
+                    .roles(user.getRoles().toArray(new String[0]))
+                    .build();
+            return userDetails;
+        }
+        throw new UsernameNotFoundException("User not found with username: " + username);
+    }
+}
