c98969cce2857a834bf909fa30e678b9a1858055
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-10-09 21:20:33 +0530

    fix: minor fix here and there and fix spring security configuration

--- src/main/java/com/masum/journalapp/journal/controller/JournalEntryController.java
+++ src/main/java/com/masum/journalapp/journal/controller/JournalEntryController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/journals")
+@RequestMapping("/journal")
 public class JournalEntryController {
 
     @Autowired
--- src/main/java/com/masum/journalapp/journal/entity/User.java
+++ src/main/java/com/masum/journalapp/journal/entity/User.java
@@ -21,7 +21,7 @@ public class User {
     private String username;
     @NonNull
     private String password;
-
     @DBRef
     private List<JournalEntry> journalEntries = new ArrayList<>();
+    private List<String> roles;
 }
